72ea158cab614f816173c814c6aada2674c6d793
foreverdevops <88678903+foreverdevops@users.noreply.github.com>
2021-10-24 16:05:00 +0300

    ..
    ...

--- Taksimetre.java
+++ Taksimetre.java
@@ -1,14 +1,16 @@
 public class Taksimetre {
+  final static float KM_UCRET = 2.2f;
 
   public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     int acilis=10-;
     float ucret,mesafe;
-    System.out.println("km?");
+  
+    System.out.println("mesafe?");
     mesafe=sc.nextInt();
     sc.close();
     
-    ucret= ( (float)acilis + mesafe * 2.2f);
+    ucret= ( (float)acilis + mesafe *KM_UCRET );
     System.out.println(ucret > 20 ? ucret + "tl" : (int)20 + " tl");
     
   
