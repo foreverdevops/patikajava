2942375d6ecce0345e75f420efea686f6e7956f0
foreverdevops <88678903+foreverdevops@users.noreply.github.com>
2022-07-26 22:12:37 +0300

    .
    .

---
+++ PassTheClass.java
@@ -0,0 +1,26 @@
+package patika;
+
+import java.util.Scanner;
+
+public class PassTheClass {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner();
+        double average;
+        int mat,turkce,fizik,kimya,muzik;
+        mat=scanner.nextInt();
+        turkce=scanner.nextInt();
+        fizik=scanner.nextInt();
+        kimya=scanner.nextInt();
+        muzik=scanner.nextInt();
+
+        average=(mat + fizik + turkce + kimya + muzik
+
+                ) / 5;
+        if (average<=55) {
+            System.out.println("sinifta kaldin.ortalama: " + average);
+        }
+        else {
+            System.out.println("sinifi gectinn.ortalama: " + average);
+        }
+    }
+}
